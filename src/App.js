e442a810095e12001ffbd7ff8a32bc8df7b4b474
Rick <rick@ricks-MacBook-Pro.local>
2022-02-23 02:17:09 +0900

    Coin display and calculator using coin API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [chosenCoin, setChosenCoin] = useState();
+  const [usd, setUsd] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setChosenCoin(json[0].quotes.USD.price);
+        setLoading(false);
+      });
+  }, []);
+  const onChange = (event) => {
+    console.log(event.target.value);
+    setChosenCoin(event.target.value);
+  };
+  const onChangeUsd = (event) => {
+    setUsd(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onChange}>
+            {coins.map((item) => (
+              <option key={item.id} value={item.quotes.USD.price}>
+                {item.name} ({item.symbol}) : {item.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          <hr />
+          <div>
+            USD :{" "}
+            <form onChange={onChangeUsd}>
+              <input value={usd}></input>
+              <button>ok</button>
+            </form>
+            result : {usd / chosenCoin}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
